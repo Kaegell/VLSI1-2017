2c438c43bca49a60e542aa7eda7fc26ae71957ad
Nicolas Phan <nicolas.van.phan@gmail.com>
2017-12-03 10:34:28 +0100

    Debug FIFOv2 and add testbench
    Now, accordingly to the testbench, the FIFOv2 works!

--- src/common/fifov2/fifov2.vhdl
+++ src/common/fifov2/fifov2.vhdl
@@ -1,18 +1,24 @@
 
 library ieee;
 use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
 
 entity fifov2 is
-  generic ( SIZE  : integer := 5
+  generic ( CAPACITY  : integer := 5;
+            LENGTH    : integer := 72
       );
-  port (din       : in  std_logic_vector(71 downto 0);
-        dout      : out std_logic_vector(71 downto 0);
+  port (din       : in  std_logic_vector((LENGTH - 1) downto 0);
+        dout      : out std_logic_vector((LENGTH - 1) downto 0);
 
         push      : in  std_logic;
         pop       : in  std_logic;
 
         full      : out std_logic;
         empty     : out std_logic;
+
+        debug_head : out std_logic_vector(3 downto 0);
+        debug_tail : out std_logic_vector(3 downto 0);
+        debug_size : out std_logic_vector(3 downto 0);
 
         reset_n   : in  std_logic;
         ck        : in  std_logic;
@@ -22,8 +28,15 @@ entity fifov2 is
 end fifov2;
 
 architecture arch of fifov2 is
-  type arr is array (0 to (SIZE - 1)) of std_logic_vector(71 downto 0);
+  type arr is array ((CAPACITY - 1) downto 0)
+  of std_logic_vector((LENGTH - 1) downto 0);
   signal fifo : arr;
+  signal s_head : std_logic_vector(3 downto 0);
+  signal s_tail : std_logic_vector(3 downto 0);
+  signal s_size : std_logic_vector(3 downto 0);
+  signal old_din : std_logic_vector((LENGTH - 1) downto 0);
+  signal old_push : std_logic;
+  signal old_pop : std_logic;
 
   -- Bool_to_logic()
   function bool_to_logic(x : boolean)
@@ -39,11 +52,15 @@ architecture arch of fifov2 is
 begin
 
 process (ck)
-  variable head : integer;
-  variable tail : integer;
-  variable size : integer;
+  variable head : integer range 0 to (CAPACITY - 1);
+  variable tail : integer range 0 to (CAPACITY - 1);
+  variable size : integer range 0 to CAPACITY;
 begin
   if (rising_edge(ck)) then
+
+    old_push <= push;
+    old_pop <= pop;
+    old_din <= din;
 
     -- === INITIALIZATION ===
     if (reset_n = '0') then
@@ -52,36 +69,54 @@ begin
       tail := 0;
       size := 0;
       -- Reset the whole array
-      for i in 0 to (SIZE - 1) loop
-        fifo(i) <= (others => '0');
+      for k in 0 to (CAPACITY - 1) loop
+        fifo(k) <= (others => '0');
       end loop;
       -- Reset output
-      full <= '0';
-      empty <= '1';
     else
       -- Process core : running the FIFO
 
       -- === PUSH ===
-      if (push = '1') then
-        fifo(head) <= din;
-        head := (head + 1) mod SIZE;
+      if (old_push = '1') then
+        fifo(head) <= old_din;
+        if (head < CAPACITY - 1) then
+          head := (head + 1);
+        else
+          head := 0;
+        end if;
         size := size + 1;
       end if;
 
       -- === POP ===
-      if (pop = '1') then
-        tail := (tail + 1) mod SIZE; 
+      if (old_pop = '1') then
+        if (tail < CAPACITY - 1) then
+          tail := (tail + 1);
+        else
+          tail := 0;
+        end if;
         size := size - 1;
       end if;
-
-      -- === Outputs ===
-      dout <= fifo(tail);
-      full <= bool_to_logic(size = SIZE);
-      -- We consider the FIFO empty if we can't pop
-      -- In other words, if after a hypothetical pop,
-      -- the tail would point to an empty slot
-      empty <= bool_to_logic((size = 0) or (size = 1 and push = '0'));
     end if;
+    -- === Outputs ===
+    s_head <= std_logic_vector(to_unsigned(head, s_head'length));
+    s_tail <= std_logic_vector(to_unsigned(tail, s_tail'length));
+    s_size <= std_logic_vector(to_unsigned(size, s_size'length));
+
   end if;
 end process;
-end architecture;
+
+debug_head <= s_head;
+debug_tail <= s_tail;
+debug_size <= s_size;
+
+dout <= fifo(to_integer(unsigned(s_tail)));
+    -- We consider the FIFO full if we can't push
+    -- In other words, if after a hypothetical push,
+    -- the case pointed by the tail would be overwritten
+full <= bool_to_logic(s_size = std_logic_vector(
+        to_unsigned(CAPACITY, s_size'length)) and pop = '0');
+    -- We consider the FIFO empty if we can't pop
+    -- In other words, if after a hypothetical pop,
+    -- the tail would point to an empty slot
+empty <= bool_to_logic((s_size = "0000") or (s_size = "0001" and push = '0'));
+end arch;
--- src/common/fifov2/fifov2_tb.vhdl
+++ src/common/fifov2/fifov2_tb.vhdl
@@ -29,6 +29,10 @@ architecture arch of fifov2_tb is
   signal full      : std_logic;
   signal empty     : std_logic;
 
+  signal debug_head : std_logic_vector(3 downto 0);
+  signal debug_tail : std_logic_vector(3 downto 0);
+  signal debug_size : std_logic_vector(3 downto 0);
+
   signal reset_n   : std_logic;
   signal ck        : std_logic;
   signal vdd       : std_logic;
@@ -44,6 +48,10 @@ architecture arch of fifov2_tb is
 
         full      : out std_logic;
         empty     : out std_logic;
+
+        debug_head : out std_logic_vector(3 downto 0);
+        debug_tail : out std_logic_vector(3 downto 0);
+        debug_size : out std_logic_vector(3 downto 0);
 
         reset_n   : in  std_logic;
         ck        : in  std_logic;
@@ -65,12 +73,108 @@ begin
         full      => full,
         empty     => empty,
 
+        debug_head => debug_head,
+        debug_tail => debug_tail,
+        debug_size => debug_size,
+
         reset_n   => reset_n,
         ck        => ck,
         vdd       => vdd,
         vss       => vss);
 
   -- =================== Stimuli generation ===================
- 
-end architecture;
+  process
+    variable v_din :  integer := 0;
+    variable v_push : boolean := false;
+    variable v_pop :  boolean := false;
+  begin
+    -- ================ Chronogramm 1 ================
+    -- Clock cycle
+    reset_n <= '0';
+    push <= '0';
+    pop <= '0';
+    wait for 50 ps;
+    ck <= '0';
+    wait for 50 ps;
+    ck <= '1';
+    --wait for 50 ps;
+    --ck <= '0';
+    --wait for 50 ps;
+    --ck <= '1';
+    reset_n <= '1';
+
+    for i in 0 to 20 loop
+      -- Generate variable values
+      v_din := 10 + i;
+      v_push := (i = 1
+      or i = 2
+      or i = 3
+      or i = 5
+      or i = 8
+      or i = 9
+      or i = 14
+      or i = 15
+      or i = 16
+      or i = 17);
+      v_pop := (i = 3
+      or i = 4
+      or i = 6
+      or i = 9
+      or i = 10
+      or i = 15
+      or i = 16);
+
+      -- Assign variables to signals
+      din <= std_logic_vector(to_unsigned(v_din, din'length));
+      push <= bool_to_logic(v_push);
+      pop <= bool_to_logic(v_pop);
+
+      assert false report "cycle";
+
+      -- Clock cycle
+      wait for 50 ps;
+      ck <= '0';
+      wait for 50 ps;
+      ck <= '1';
+    end loop;
+
+    -- ================ Chronogramm 2 ================
+    -- Clock cycle
+    reset_n <= '0';
+    push <= '0';
+    pop <= '0';
+    wait for 50 ps;
+    ck <= '0';
+    wait for 50 ps;
+    ck <= '1';
+    --wait for 50 ps;
+    --ck <= '0';
+    --wait for 50 ps;
+    --ck <= '1';
+    reset_n <= '1';
+
+    for i in 0 to 20 loop
+      -- Generate variable values
+      v_din := 10 + i;
+      v_push := ( (i > 1 and i < 13) );
+      v_pop := ( (i > 2 and i < 9)
+      or (i > 14 and i < 19));
+
+      -- Assign variables to signals
+      din <= std_logic_vector(to_unsigned(v_din, din'length));
+      push <= bool_to_logic(v_push);
+      pop <= bool_to_logic(v_pop);
+
+      assert false report "cycle";
+
+      -- Clock cycle
+      wait for 50 ps;
+      ck <= '0';
+      wait for 50 ps;
+      ck <= '1';
+    end loop;
+    wait;
+  end process;
+
+end arch;
 
